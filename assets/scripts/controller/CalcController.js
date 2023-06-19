28bcdc0ca6dd02bcc6df6c9064dcf235d6ba5b10
EDUDESTROER <eduardopicholi@hotmail.com>
2023-06-19 20:43:23 -0300

    Adding click events to buttons and creating functions that bring the system time and date, according to the browser language.

--- assets/scripts/controller/CalcController.js
+++ assets/scripts/controller/CalcController.js
@@ -2,34 +2,86 @@ class CalcController {
 
     constructor(){
 
-        this._displayCalc = '0';
+        this._locale = navigator.language;
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#date");
+        this._hourEl = document.querySelector("#hour");
         this._currentDate;
         this.initialize();
+        this.initButtonsevents();
 
     }
 
     initialize(){
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#date");
-        let hourEl = document.querySelector("#hour");
+        this.setDisplayDateTime();
 
-        displayCalcEl.value = 7654;
-        dateEl.innerHTML = "18/06/2023"
-        hourEl.innerHTML = "22:01";
+       setInterval(()=>{
 
+        this.setDisplayDateTime();
+
+       }, 1000);
+
+    }
+
+    addEventListenerALL(element, events, fn){
+
+        events.split(' ').forEach(action=>{
+            element.addEventListener(action, fn, false);
+        });
+
+    }
+
+    initButtonsevents(){
+
+        let buttons = document.querySelectorAll('.wrapper-buttons-queues > button');
+        buttons.forEach((btn, index)=>{
+            
+            this.addEventListenerALL(btn,'click drag', e=>{
+
+                console.log(btn.id.replace("btn-", ""));
+    
+            });
+        });
+
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayHour = this.currentDate.toLocaleTimeString(this._locale,{
+            hour: "2-digit",
+            minute: "2-digit"
+        });
+
+    }
+
+    get displayHour(){
+        return this._hourEl.innerHTML;
+    }
+
+    set displayHour(value){
+        return this._hourEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl;
     }
     
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalcEl = value;
     }
 
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
 
     set currentDate(value){
