8ea9135aeed9d35ea810c86bceb835f77805a6b5
EDUDESTROER <eduardopicholi@hotmail.com>
2023-06-20 20:21:36 -0300

    dealing with arrays and creating a switch that handles operators and numbers, in addition to methods.

--- assets/scripts/controller/CalcController.js
+++ assets/scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor(){
 
+        this._operation = [];
         this._locale = navigator.language;
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#date");
@@ -27,8 +28,165 @@ class CalcController {
     addEventListenerALL(element, events, fn){
 
         events.split(' ').forEach(action=>{
-            element.addEventListener(action, fn, false);
+            element.addEventListener(action, fn);
         });
+
+    }
+
+    clearALL(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    getLastOperation(){
+
+         return this._operation[this._operation.length-1];
+
+    }
+
+    setError(){
+
+        this.displayCalc.value = "ERROR!"
+
+    }
+    setLastOperation(value){
+
+        this._operation[this._operation.length-1] = value;
+
+    }
+
+    setLastNumberToDisplay(){
+
+        
+
+    }
+
+    isOperator(value){
+
+        return (['%', '*','-','+','/'].indexOf(value) > -1);
+
+    }
+
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if(this._operation.length > 3){
+
+            this.calc();
+
+        }
+
+    }
+
+    calc(){
+
+        let last = this._operation.pop();
+
+        let result = eval(this._operation.join(""));
+
+        this._operation = [result, last];
+
+    }
+
+    addOperation(value){
+
+        console.log('Coisa ',value, isNaN(this.getLastOperation()));
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)){
+
+                this.setLastOperation(value);
+
+            }else if(isNaN(value)){
+
+                console.log('Outra Coisa ',value);
+
+            }else {
+
+                this.pushOperation(value);
+                console.log(value);
+
+            }
+
+        }else {
+
+            if(this.isOperator(value)){
+
+                this.pushOperation(value);
+
+            }else{
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+
+            }
+
+        }
+
+    }
+
+    execBtn(value){
+
+        switch (value){
+            case 'percent':
+                this.addOperation('%');
+                break;
+            case 'division':
+                this.addOperation('/');
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'c':
+                this.clearALL();
+                break;
+            case 'multiplication':
+                this.addOperation('*');
+                break;
+            case 'subtraction':
+                this.addOperation('-');
+                break;
+            case 'sum':
+                this.addOperation('+');
+                break;
+            case 'change-sing':
+                this.addOperation('+/-');
+                break;
+            case 'point':
+                this.addOperation('.');
+                break;
+            case 'equal':
+                
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            
+            default:
+                this.setError();  
+                break;
+            
+        }
 
     }
 
@@ -39,7 +197,9 @@ class CalcController {
             
             this.addEventListenerALL(btn,'click drag', e=>{
 
-                console.log(btn.id.replace("btn-", ""));
+                let textBtn = btn.id.replace("btn-", "");
+
+                this.execBtn(textBtn);
     
             });
         });
